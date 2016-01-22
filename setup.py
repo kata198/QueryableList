87953f1e22239d2f159e2baeccdc4ce5ea48a040
Tim Savannah <kata198@gmail.com>
2016-01-22 13:40:18 -0500

    2.0.0 in changelog and setup

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ if __name__ == '__main__':
         long_description = summary
 
     setup(name='QueryableList',
-            version='1.2.1',
+            version='2.0.0',
             packages=['QueryableList'],
             author='Tim Savannah',
             author_email='kata198@gmail.com',
