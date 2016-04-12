8ab9651108742f643b9a74bed9404534501b0845
Tim Savannah <kata198@gmail.com>
2016-04-12 00:29:12 -0400

    bump version

--- QueryableList/__init__.py
+++ QueryableList/__init__.py
@@ -177,8 +177,8 @@ Pydoc documentation can be found at: http://htmlpreview.github.io/?https://githu
 
 __all__ = ('FILTER_TYPES', 'FILTER_METHOD_OR', 'FILTER_METHOD_OR', 'FILTER_METHODS', 'QueryableListObjs', 'QueryableListDicts', 'QueryableListBase', 'QueryableListMixed', 'QueryBuilder')
 
-__version__ = '2.0.0'
-__version_tuple__ = (2, 0, 0)
+__version__ = '2.1.0'
+__version_tuple__ = (2, 1, 0)
 
 
 from .constants import FILTER_TYPES, FILTER_METHODS, FILTER_METHOD_OR, FILTER_METHOD_OR
