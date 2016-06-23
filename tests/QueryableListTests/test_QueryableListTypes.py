28af26108df7123aa38ddbf973233aad682cfa82
Tim Savannah <kata198@gmail.com>
2016-06-23 00:14:40 -0400

    Add runTests for GoodTests, and add a few basic tests for now.

---
+++ tests/QueryableListTests/test_QueryableListTypes.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env GoodTests.py
+
+import sys
+import subprocess
+
+from QueryableList import QueryableListObjs, QueryableListDicts, QueryableListMixed
+
+
+class DataObject(object):
+
+    def __init__(self, **kwargs):
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+class TestQueryableListTypes(object):
+
+    def setup_class(self):
+        self.dataObjs = [
+            DataObject(a='one', b='two'),
+            DataObject(a='one', b='five'),
+            DataObject(a='six', c='eleven'),
+        ]
+
+        self.dataDicts = [ 
+            { 'a' : 'one', 'b' : 'two'}, 
+            { 'a' : 'one', 'b' : 'five'}, 
+            { 'a' : 'six', 'c' : 'eleven'},
+        ]
+
+
+    def test_Objs(self):
+
+        gotException = False
+
+        try:
+            qlObjs = QueryableListObjs(self.dataObjs)
+            found = qlObjs.filter(a='one')
+        except Exception as e:
+            gotException = e
+
+        assert gotException is False, 'Got Exception for QueryableListObjs when should not have: %s%s' %(str(type(e)), str(e)) 
+        assert len(found) == 2, 'Did not find correct number of items'
+
+        gotException = False
+        try:
+            dObjs = QueryableListObjs(self.dataDicts)
+            found = dObjs.filter(a='one')
+        except Exception as e:
+            # I personally think this should raise an exception, so test is written like this,
+            #  but it would be too performant loss to check every time.
+            gotException = e
+
+        #assert gotException is not False, 'Expected to get exception, but did not.'
+        assert len(found) == 0, 'Expected not to find any items when using QueryableListObjs with list of dicts'
+
+    def test_Dicts(self):
+
+        gotException = False
+
+        try:
+            qlDicts = QueryableListDicts(self.dataDicts)
+            found = qlDicts.filter(a='one')
+        except Exception as e:
+            gotException = e
+
+        assert gotException is False, 'Got Exception for QueryableListDicts when should not have: %s%s' %(str(type(e)), str(e)) 
+        assert len(found) == 2, 'Did not find correct number of items'
+
+        gotException = False
+        try:
+            dDicts = QueryableListObjs(self.dataDicts)
+            found = dDicts.filter(a='one')
+        except Exception as e:
+            # I personally think this should raise an exception, so test is written like this,
+            #  but it would be too performant loss to check every time.
+            gotException = e
+
+        #assert gotException is not False, 'Expected to get exception, but did not.'
+        assert len(found) == 0, 'Expected not to find any items when using QueryableListDicts with list of objs'
+
+
+    def testMixed(self):
+        gotException = False
+
+        try:
+            qlDicts = QueryableListMixed(self.dataDicts)
+            found = qlDicts.filter(a='one')
+        except Exception as e:
+            gotException = e
+
+        assert gotException is False, 'Got Exception for QueryableListMixed on objects when should not have: %s%s' %(str(type(e)), str(e)) 
+        assert len(found) == 2, 'Did not find correct number of items'
+
+        gotException = False
+        try:
+            dDicts = QueryableListMixed(self.dataDicts)
+            found = dDicts.filter(a='one')
+        except Exception as e:
+            gotException = e
+
+        assert gotException is False, 'Got Exception for QueryableListMixed on dicts when should not have: %s%s' %(str(type(e)), str(e)) 
+        assert len(found) == 2, 'Did not find correct number of items'
+
+
+
+
+if __name__ == '__main__':
+    sys.exit(subprocess.Popen('GoodTests.py "%s"' %(sys.argv[0],), shell=True).wait())
---
+++ tests/runTests.py
@@ -0,0 +1,196 @@
+#!/usr/bin/env python
+#
+#  Copyright (c) 2015 Tim Savannah under following terms:
+#   You may modify and redistribe this script with your project
+#
+# It will download the latest GoodTests.py and use it to execute the tests.
+#
+#  This should be placed in a directory, "tests", at the root of your project. It assumes that ../$MY_PACKAGE_MODULE is the path to your test module, and will create a symlink to it in order to run tests.
+#  The tests should be found in $MY_TEST_DIRECTORY in given "tests" folder.
+
+import imp
+import os
+
+import glob
+import subprocess
+import sys
+
+GOODTESTS_URL = 'https://raw.githubusercontent.com/kata198/GoodTests/master/GoodTests.py'
+
+# This should be your module name, and can be any relative or absolute path, or just a module name. 
+# If just a module name is given, the directory must be in current directory or parent directory.
+MY_PACKAGE_MODULE = 'QueryableList'
+
+#  Normally, you want to test the codebase during development, so you don't care about the site-packages installed version.
+#     If you want to allow testing with any module by @MY_PACKAGE_MODULE in the python path, change this to True.
+ALLOW_SITE_INSTALL = False
+
+# This is the test directory that should contain all your tests. This should be a directory in your "tests" folder
+MY_TEST_DIRECTORY = 'QueryableListTests'
+
+def findGoodTests():
+    pathSplit = os.environ['PATH'].split(':')
+    if '.' not in pathSplit:
+        pathSplit = ['.'] + pathSplit
+        os.environ['PATH'] = ':'.join(pathSplit)
+
+    result = ''
+    success = False
+    for path in pathSplit:
+        if path.endswith('/'):
+            path = path[:-1]
+        guess = path + '/GoodTests.py'
+        if os.path.exists(guess):
+            success = True
+            result = guess
+            break
+
+    return {
+        'path'    : result,
+        "success" : success 
+    }
+
+def download_goodTests():
+    validAnswer = False
+    while validAnswer == False:
+        sys.stdout.write('GoodTests not found. Would you like to install it to local folder? (y/n): ')
+        sys.stdout.flush()
+        answer = sys.stdin.readline().strip().lower()
+        if answer not in ('y', 'n', 'yes', 'no'):
+            continue
+        validAnswer = True
+        answer = answer[0]
+
+    if answer == 'n':
+        sys.stderr.write('Cannot run tests without installing GoodTests. http://pypi.python.org/pypi/GoodTests or https://github.com/kata198/Goodtests\n')
+        sys.exit(1)
+    try:
+        import urllib2 as urllib
+    except ImportError:
+        try:
+            import urllib.request as urllib
+        except:
+            sys.stderr.write('Failed to import urllib. Trying pip.\n')
+            import subprocess
+            pipe = subprocess.Popen('pip install GoodTests', shell=True)
+            res = pipe.wait()
+            if res != 0:
+                sys.stderr.write('Failed to install GoodTests with pip ordirect download. aborting.\n')
+                sys.exit(1)
+    try:
+        response = urllib.urlopen(GOODTESTS_URL)
+        contents = response.read()
+        if str != bytes:
+            contents = contents.decode('ascii')
+    except Exception as e:
+        sys.stderr.write('Failed to download GoodTests.py from "%s"\n%s\n' %(GOODTESTS_URL, str(e)))
+        sys.exit(1)
+    try:
+        with open('GoodTests.py', 'w') as f:
+            f.write(contents)
+    except Exception as e:
+        sys.stderr.write('Failed to write to GoodTests.py\n%s\n' %(str(e,)))
+        sys.exit(1)
+    try:
+        os.chmod('GoodTests.py', 0o775)
+    except:
+        sys.stderr.write('WARNING: Failed to chmod +x GoodTests.py, may not be able to be executed.\n')
+
+    try:
+        import GoodTests
+    except ImportError:
+        sys.stderr.write('Seemed to download GoodTests okay, but still cannot import. Aborting.\n')
+        sys.exit(1)
+    
+
+if __name__ == '__main__':
+
+    thisDir = os.path.dirname(__file__)
+    if not thisDir:
+        thisDir = str(os.getcwd())
+    elif not thisDir.startswith('/'):
+        thisDir = str(os.getcwd()) + '/' + thisDir
+
+    # If GoodTests is in current directory, make sure we find it later
+    if os.path.exists('./GoodTests.py'):
+        os.environ['PATH'] = str(os.getcwd()) + ':' + os.environ['PATH']
+
+    os.chdir(thisDir)
+
+    didDownload = False
+    
+    goodTestsInfo = findGoodTests()
+    if goodTestsInfo['success'] is False:
+        download_goodTests()
+        goodTestsInfo = findGoodTests()
+        if goodTestsInfo['success'] is False:
+            sys.stderr.write('Could not download or find GoodTests.py. Try to download it yourself using "pip install GoodTests", or wget %s\n' %( GOODTESTS_URL,))
+            sys.exit(1)
+
+    PYTHON_ENDINGS = ('.py', '.pyc', '.pyo')
+
+    origMyPackageModule = MY_PACKAGE_MODULE[:]
+    baseName = os.path.basename(MY_PACKAGE_MODULE)
+    dirName = os.path.dirname(MY_PACKAGE_MODULE)
+    
+    newPath = None
+    if dirName not in ('.', ''):
+        if dirName.startswith('.'):
+            dirName = os.getcwd() + os.sep + dirName + os.sep
+        newPath = dirName
+    elif dirName == '':
+        inCurrentDir = False
+        try:
+            imp.find_module(MY_PACKAGE_MODULE)
+            inCurrentDir = True
+        except ImportError:
+            # COMPAT WITH PREVIOUS runTests.py: Try plain module in parent directory
+            foundIt = False
+            try:
+                imp.find_module('..' + os.sep + MY_PACKAGE_MODULE)
+                foundIt = True
+            except ImportError:
+                if not ALLOW_SITE_INSTALL:
+                    sys.stderr.write('Cannot find "%s" locally.\n' %(MY_PACKAGE_MODULE,))
+                    sys.exit(2)
+                else:
+                    try:
+                        __import__(baseName)
+                    except:
+                        sys.stderr.write('Cannot find "%s" locally or in global python path.\n' %(MY_PACKAGE_MODULE,))
+                        sys.exit(2)
+
+            if foundIt is True:
+                newPath = os.getcwd() + os.sep + '..' + os.sep
+        if inCurrentDir is True:
+            newPath = os.getcwd() + os.sep + '..' + os.sep
+    
+    if newPath:
+        newPythonPath = [newPath] + [x for x in os.environ.get('PYTHONPATH', '').split(':') if x]
+        os.environ['PYTHONPATH'] = ':'.join(newPythonPath)
+        sys.path = [newPath] + sys.path
+
+    try:
+        __import__(baseName)
+    except ImportError as e:
+        if baseName.endswith(('.py', '.pyc', '.pyo')):
+            MY_PACKAGE_MODULE = baseName[baseName.rindex('.')]
+
+        if e.name != MY_PACKAGE_MODULE:
+            sys.stderr.write('Error while importing %s: %s\n Likely this is another dependency that needs to be installed\nPerhaps run "pip install %s" or install the providing package.\n\n' %(e.name, str(e), e.name))
+            sys.exit(1)
+        sys.stderr.write('Could not import %s. Either install it or otherwise add to PYTHONPATH\n%s\n' %(MY_PACKAGE_MODULE, str(e)))
+        sys.exit(1)
+
+    if not os.path.isdir(MY_TEST_DIRECTORY):
+        if not os.path.exists(MY_TEST_DIRECTORY):
+            sys.stderr.write('Cannot find test directory: %s\n' %(MY_TEST_DIRECTORY,))
+        else:
+            sys.stderr.write('Provided test directory, "%s" is not a directory.\n' %(MY_TEST_DIRECTORY,))
+        sys.exit(3)
+
+    sys.stdout.write('Starting test..\n')
+    sys.stdout.flush()
+    sys.stderr.flush()
+    pipe = subprocess.Popen([goodTestsInfo['path'], MY_TEST_DIRECTORY], env=os.environ, shell=False)
+    pipe.wait()
