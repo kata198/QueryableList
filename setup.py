db1f186b689d5d867fd11f57ebb9bccf19d70592
Tim Savannah <kata198@gmail.com>
2017-04-23 23:25:45 -0400

    3.1.0

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ if __name__ == '__main__':
         long_description = summary
 
     setup(name='QueryableList',
-            version='3.0.1',
+            version='3.1.0',
             packages=['QueryableList'],
             author='Tim Savannah',
             author_email='kata198@gmail.com',
